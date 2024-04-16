9a6deb72d2ce688cddc815b45d07db5d1a1ad921
johnnataa <johnnata_@outlook.com>
2024-04-16 12:44:02 -0300

    Enviando projeto

---
+++ app/main.js
@@ -0,0 +1,11 @@
+let livros = [];
+const endpointAPI =
+  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+getBuscarLivrosDaAPI();
+
+async function getBuscarLivrosDaAPI() {
+  const res = await fetch(endpointAPI);
+  livros = await res.json();
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirLivrosNaTela(livrosComDesconto);
+}
